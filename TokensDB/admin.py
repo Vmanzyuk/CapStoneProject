724fe39d5956434846897abe7311e16034e53e35
Vladimir <vmanzyuk@outlook.com>
2018-10-08 23:10:14 +0300

    add buttons to fill and delete data from coinmarketcap in admins panel

--- TokensDB/admin.py
+++ TokensDB/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponseRedirect
 
 from .models import Token
 
-admin.site.register(Token)
+
+class MyModelAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('deletealltokens/', self.admin_site.admin_view(self.delete_all_tokens)),
+            path('firstfill/', self.admin_site.admin_view(self.first_fill))
+        ]
+        return my_urls + urls
+
+    def delete_all_tokens(self, request):
+        self.model.objects.all().update(name='delete')
+        self.message_user(request, "All tokens was deleted")
+        return HttpResponseRedirect("../")
+
+    def first_fill(self, request):
+        self.model.objects.all().update(name='fill')
+        self.message_user(request, "Sucessful insert from CMC")
+        return HttpResponseRedirect("../")
+
+
+
+
+class TokenAdmin(MyModelAdmin):
+	change_list_template="entities/tokens_changelist.html"
+
+admin.site.register(Token,TokenAdmin)
 
 # Register your models here.
